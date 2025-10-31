4df76c51e19fa3c9aa6fbfae5755b2a1eb5ff34f
Junior Fernando <juniorfernandojrf@gmail.com>
2025-10-31 10:08:29 +0100

    feat: criar seeders do user para popular dados na base de dados

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::where('name', 'user')->first();
+
+        User::updateOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Usuario do Sistema',
+                'password' => Hash::make('Teste123@'),
+                'role_id' => $adminRole->id,
+            ]
+        );
+    }
+}
